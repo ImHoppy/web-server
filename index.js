5b2be478ee9d97a21e75746c0a9b04331202fc89
Hoppy <mbraets@student.42.fr>
2022-01-06 23:45:48 +0000

    add get distance from last point and todo delete duplicate

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ server.add("/gps", (req, res) => {
 })
 
 class Pos {
-	constructor(longitude, latitude, date) {
-		this.date = date;
-		this.longitude = longitude;
+	constructor(latitude, longitude) {
+		this.date = Date.now();
 		this.latitude = latitude;
+		this.longitude = longitude;
 	}
 	
 	get getCity() {
@@ -25,12 +25,44 @@ class Pos {
 	
 }
 
-server.add("/", (req, res, url) => {
-	console.log(url.href)
+function getDistanceFromLatLonInKm(pos1, pos2) {
+	var R = 6371; // Radius of the earth in km
+	var dLat = deg2rad(pos2.latitude-pos1.latitude);
+	var dLon = deg2rad(pos2.longitude-pos1.longitude); 
+	var a = 
+	Math.sin(dLat/2) * Math.sin(dLat/2) +
+	Math.cos(deg2rad(pos1.latitude)) * Math.cos(deg2rad(pos2.latitude)) * 
+	Math.sin(dLon/2) * Math.sin(dLon/2)
+	; 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c; // km
+	return d;
+}
+
+function deg2rad(deg) {
+	return deg * (Math.PI/180)
+}
+
+server.add("/gps/post", async (req, res, url) => {
+	if (!req.method.includes("POST")) {
+		res.writeHead(405, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify({status: 405, data: 'Hello User!'}));
+		return;
+	}
+
+	// GET BODY
+	const buffers = [];
+	for await (const chunk of req)
+		buffers.push(chunk);
+	const body = JSON.parse(Buffer.concat(buffers).toString());
+
+	let newpos = new Pos(body.latitude, body.longitude);
 	let rawdata = fs.readFileSync('gps/pos.json');
-	let pos = JSON.parse(rawdata);
-	pos.push(new Pos(10, 10, Date.now()));
-	fs.writeFileSync("gps/pos.json", JSON.stringify(pos, undefined, 2));
+	let data = JSON.parse(rawdata);
+	console.log(getDistanceFromLatLonInKm(new Pos(data[data.lenght - 1].latitude, data[data.lenght - 1].longitude), newpos));
+	// if (getDistanceFromLatLonInKm(data[data.lenght - 1], newpos) > 1)
+		// data.push(new Pos(body.latitude, body.longitude));
+	fs.writeFileSync("gps/pos.json", JSON.stringify(data, undefined, 2));
 	res.writeHead(200, { 'Content-Type': 'application/json' });
-	res.end(JSON.stringify({data: 'Hello Android!'}));
+	res.end(JSON.stringify({status: 200, data: 'Hello Android!'}));
 });
